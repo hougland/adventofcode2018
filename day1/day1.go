fa62b047ceb0ebd51ce71071f88cede9bdd88606
Ricky Hougland <houglande@gmail.com>
2018-12-01 20:35:05 -0800

    day 1 solutions - part 1 and 2

--- day1/day1.go
+++ day1/day1.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main()  {
-	fmt.Print(something)
+	pwd, _ := os.Getwd()
+	numbers := readFile(pwd + "/day1/input.txt")
+
+	foundRepeat := false
+	m := make(map[int]bool)
+	total := 0
+	m[total] = true
+
+	for foundRepeat != true {
+		for _, num := range numbers {
+			total += num
+
+			if _, ok := m[total]; ok { // key found
+				foundRepeat = true
+				fmt.Print(num)
+				break
+			} else { // key not found
+				m[total] = true
+			}
+		}
+	}
+
+	fmt.Print("\n")
+	fmt.Print(total)
+}
+
+func readFile(filePath string) (numbers []int) {
+	fd, err := os.Open(filePath)
+	if err != nil {
+		panic(fmt.Sprintf("open %s: %v", filePath, err))
+	}
+	var line int
+	for {
+		_, err := fmt.Fscanf(fd, "%d\n", &line)
+
+		if err != nil {
+			fmt.Println(err)
+			if err == io.EOF {
+				return
+			}
+			panic(fmt.Sprintf("Scan Failed %s: %v", filePath, err))
+
+		}
+		numbers = append(numbers, line)
+	}
+	return
 }
